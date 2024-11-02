5bc7347402c7d817a3c48b6e631649ec42dcf965
sahiem <sahieemhb@gmail.com>
2024-11-03 00:00:56 +0530

    Initial commit with website files

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth",
-   });
-  });
-});
+let currentSlide = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function moveSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides; // Move to the next slide
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// Set the interval for auto sliding (every 3 seconds)
+setInterval(moveSlide, 3000); // Change slides every 3 seconds
